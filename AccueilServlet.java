9cbdf48cc1745bde58393534c95322afcb8f7d98
rambo53 <71452106+rambo53@users.noreply.github.com>
2021-08-23 16:59:30 +0200

    Create AccueilServlet.java

---
+++ AccueilServlet.java
@@ -0,0 +1,43 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AccueilServlet
+ */
+@WebServlet(name = "AccueilServlet", urlPatterns = {"/accueil"})
+public class AccueilServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public AccueilServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		  RequestDispatcher disp = request.getRequestDispatcher("/accueil.jsp");
+		  disp.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+		
+		  RequestDispatcher disp = request.getRequestDispatcher("/accueil.jsp");
+		  disp.forward(request, response);
+	}
+
+}
