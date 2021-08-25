4f30aba1cf7aa471611e75d35461c1e294ca15e2
rambo53 <71452106+rambo53@users.noreply.github.com>
2021-08-25 16:42:25 +0200

    Update AccueilServlet.java

--- AccueilServlet.java
+++ AccueilServlet.java
@@ -1,6 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package servlet;
 
 import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,61 +17,50 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class AccueilServlet
+ *
+ * @author user
  */
-@WebServlet(name = "AccueilServlet", urlPatterns = {"/accueil"})
+@WebServlet(name = "AccueilServlet", urlPatterns = {"/AccueilServlet"})
 public class AccueilServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public AccueilServlet() {
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
-		//je charge la liste d'article à afficher en fonction param, par defaut toutes encheres en cours
-		
-		  RequestDispatcher disp = request.getRequestDispatcher("/accueil.jsp");
-		  ArticleVenduManager articleVenduManager = ArticleVenduManager.getInstance();
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //je charge la liste d'article à afficher en fonction param, par defaut toutes encheres en cours
+        
+		  RequestDispatcher disp = request.getRequestDispatcher("/WEB-INF/accueil.jsp");
+		 // ArticleVenduManager articleVenduManager = ArticleVenduManager.getInstance();
 		  
-		  List<ArticleVendu> lstArticles = new ArrayList<>();
-		  lstArticles=articleVenduManager.getArticlesEnCours();
+		  //List<ArticleVendu> lstArticles = new ArrayList<>();
+		  //lstArticles=articleVenduManager.getArticlesEnCours();
 		  
-		  request.setAttribute("lstArticles", lstArticles);
+		  //request.setAttribute("lstArticles", lstArticles);
 		  
 		  disp.forward(request, response);
-	}
+    }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-		
-		 //je récupère les param de mon form de recherche d'article, je charge la list d'article et je réaffiche ma page
-		
-		  RequestDispatcher disp = request.getRequestDispatcher("/accueil.jsp");
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            
+        //je récupère les param de mon form de recherche d'article, je charge la list d'article et je réaffiche ma page
+        
+		  RequestDispatcher disp = request.getRequestDispatcher("/WEB-INF/accueil.jsp");
 		  
 		  String contient=request.getParameter("contient");
 		  String categorie=request.getParameter("categorie");
 		  
-		  ArticleVenduManager articleVenduManager = ArticleVenduManager.getInstance();
+		 // ArticleVenduManager articleVenduManager = ArticleVenduManager.getInstance();
 		  
-		  List<ArticleVendu> lstArticles = new ArrayList<>();
+		  //List<ArticleVendu> lstArticles = new ArrayList<>();
 		  
 		  //penser surcharge de méthode pour getArticles!!!
-		
-		// voir pour list d'arraylist d'article, affichage avec boucles foreach imbriquées
 		  
-		  lstArticles=articleVenduManager.getArticles(contient, categorie);
+		  //lstArticles=articleVenduManager.getArticles(contient, categorie);
 		  
-		  disp.forward(request, response);;
-	}
+		  disp.forward(request, response);
+    }
 
 }
