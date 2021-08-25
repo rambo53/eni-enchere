aba4f4938716be07fc72dad8f12fe1d25ce462d7
rambo53 <71452106+rambo53@users.noreply.github.com>
2021-08-25 16:51:36 +0200

    Update NouvelleVenteServlet.java

--- NouvelleVenteServlet.java
+++ NouvelleVenteServlet.java
@@ -1,6 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,32 +15,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class NouvelleVenteServlet
+ *
+ * @author user
  */
-@WebServlet("/NouvelleVenteServlet")
+@WebServlet(name = "NouvelleVenteServlet", urlPatterns = {"/NouvelleVenteServlet"})
 public class NouvelleVenteServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public NouvelleVenteServlet() {
-        // TODO Auto-generated constructor stub
+   
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+        RequestDispatcher disp = request.getRequestDispatcher("/WEB-INF/nouvelleVente.jsp");
+	disp.forward(request, response);
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher disp = request.getRequestDispatcher("/nouvelleVente.jsp");
-		disp.forward(request, response);
-	}
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	}
+   
 
 }
