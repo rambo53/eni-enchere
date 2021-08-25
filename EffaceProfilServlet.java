6881622e127b475d2c01c3d242aeddc6ed147735
rambo53 <71452106+rambo53@users.noreply.github.com>
2021-08-25 16:44:56 +0200

    Create EffaceProfilServlet.java

---
+++ EffaceProfilServlet.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author user
+ */
+@WebServlet(name = "EffaceProfilServlet", urlPatterns = {"/EffaceProfilServlet"})
+public class EffaceProfilServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        //pas sûr param dispatcher
+        RequestDispatcher disp = request.getRequestDispatcher("AccueilServlet");
+	disp.forward(request, response);
+    }
+
+
+}
