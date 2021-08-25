341dc84cc274f91ed60b9c6a5dd426becc39a4db
rambo53 <71452106+rambo53@users.noreply.github.com>
2021-08-25 16:45:46 +0200

    Create EffaceVenteServlet.java

---
+++ EffaceVenteServlet.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author user
+ */
+@WebServlet(name = "EffaceVenteServlet", urlPatterns = {"/EffaceVenteServlet"})
+public class EffaceVenteServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        RequestDispatcher disp = request.getRequestDispatcher("/WEB-INF/accueil.jsp");
+        disp.forward(request, response);
+    }
+
+
+
+}
